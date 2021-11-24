6868e05377b9756c59e7f9232b4eb6d05edd3103
Jose Laureano <jlaureano@veilsun.com>
2021-11-24 11:54:04 -0500

    jackson dates

--- src/main/java/com/veilsun/constructkey/config/JacksonConfig.java
+++ src/main/java/com/veilsun/constructkey/config/JacksonConfig.java
@@ -2,8 +2,11 @@ package com.veilsun.constructkey.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @Configuration
@@ -13,5 +16,13 @@ public class JacksonConfig {
 	public Jackson2ObjectMapperBuilder configureObjectMapper() {
 	    return new Jackson2ObjectMapperBuilder()
 	        .modulesToInstall(Hibernate5Module.class);
+	}
+	
+	@Bean
+	@Primary
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+	    ObjectMapper objectMapper = builder.build();
+	    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+	    return objectMapper;
 	}
 }
