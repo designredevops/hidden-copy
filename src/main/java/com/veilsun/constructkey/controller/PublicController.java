36bedb398f86b561207186226c6a5b254334fb1e
Jose Laureano <jlaureano@veilsun.com>
2021-12-09 10:47:35 -0400

    adding health check endpoint

--- src/main/java/com/veilsun/constructkey/controller/PublicController.java
+++ src/main/java/com/veilsun/constructkey/controller/PublicController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +27,10 @@ public class PublicController {
 	@PostMapping("/register")
 	public ResponseEntity<?> createUser(@RequestBody UserRegistration registration) {
 		return new ResponseEntity<User>(publicService.registerUser(registration), HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/health")
+	public ResponseEntity<String> health() {
+		return new ResponseEntity<String>("Ok", HttpStatus.OK);
 	}
 }
